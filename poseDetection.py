dd1ff64a97b7151268e1c3c534535033787168f0
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-04-15 22:45:44 +0300

    angles between connection points are printed and finalized. Next move will be calculation of shoulder-elbow angle which requires a little bit thinking...

--- poseDetection.py
+++ poseDetection.py
@@ -10,19 +10,19 @@ pose = mp_pose.Pose()
 # Define landmark indices and their corresponding names
 landmark_mapping = {
     11: "LEFT SHOULDER",
-#    12: "RIGHT SHOULDER",
+    12: "RIGHT SHOULDER",
     13: "LEFT ELBOW",
-#    14: "RIGHT ELBOW",
-#    15: "LEFT WRIST",
-#    16: "RIGHT WRIST"
+    14: "RIGHT ELBOW",
+    15: "LEFT WRIST",
+    16: "RIGHT WRIST"
 }
 
 connections = [
-    (11, 13)  # Left shoulder to left elbow
-#    (13, 15),  # Left elbow to left wrist
-#    (12, 14),  # Right shoulder to right elbow
-#    (14, 16),  # Right elbow to right wrist
-#    (11, 12),  # Left shoulder to right shoulder
+    (11, 13),  # Left shoulder to left elbow
+    (13, 15),  # Left elbow to left wrist
+    (12, 14),  # Right shoulder to right elbow
+    (14, 16),  # Right elbow to right wrist
+    (11, 12)  # Left shoulder to right shoulder
 ]
 # Take video input for pose detection
 cap = cv2.VideoCapture(0)  # You can put here video of your choice ("sampleVideo.mp4")
@@ -46,7 +46,7 @@ while True:
             y = int(landmark.y * img.shape[0])
             cv2.circle(img, (x, y), 6, (0, 0, 255), 6)
             # Print landmark name and coordinates
-            print(f"{landmark_name}:\t ({x}, {y})")
+            print(f"{landmark_name}\t: ({x}, {y})")
             
             # Draw connections
         for connection in connections:
@@ -60,8 +60,8 @@ while True:
             
             angle = np.degrees(np.arctan2(end_y - start_y, end_x - start_x))
             angle = angle + 360 if angle < 0 else angle
-            print("Angle is : {}".format(angle))
-            
+            print("{}\t- {}\t: {}".format(landmark_mapping.get(connection[0]), landmark_mapping.get(connection[1]),angle))
+            #(landmark_mapping.get(connection[0]), landmark_mapping.get(connection[1]))
             cv2.line(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 4)
             
         
