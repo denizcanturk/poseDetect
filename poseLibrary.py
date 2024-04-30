22995d4ea618262fadc2062478d00a5f10d387af
Deniz CANTÜRK <denizinciebru@gmail.com>
2024-04-30 12:17:36 +0300

    Seperate angle calculation is complete. Added to a container. TODO list created..gitignore file created.

--- poseLibrary.py
+++ poseLibrary.py
@@ -5,28 +5,29 @@ import numpy as np
 #-----------------------------------------------
 
 class RollingAverageFilter:
-    def __init__(self, window_size):
+    def __init__(self, window_size:int):
         self.window_size = window_size
         self.values = []
 
-    def add_value(self, value):
+    def add_value(self, value:float):
         self.values.append(value)
         if len(self.values) > self.window_size:
             self.values.pop(0)
 
-    def get_filtered_value(self):
+    def get_filtered_value(self)->int:
         if len(self.values) == 0:
             return 0  # Return 0 if there are no values in the window
-        return sum(self.values) / len(self.values)
+        return int(sum(self.values) / len(self.values))
     
 #-----------------------------------------------
 
 class PoseDetector:
-    def __init__(self, window_size=5):
+    def __init__(self, window_size:int=5):
         self.mp_pose = mp.solutions.pose
         self.mp_draw = mp.solutions.drawing_utils
         self.pose = self.mp_pose.Pose()
         self.window_size = window_size
+        self.angleContainer = []
         self.landmark_mapping = {
             11: "LEFT SHOULDER",
             12: "RIGHT SHOULDER",
@@ -47,7 +48,19 @@ class PoseDetector:
         self.rightShoulderToElbowMvgAvg = RollingAverageFilter(window_size)
         self.righttElbowToWristMvgAvg = RollingAverageFilter(window_size)
 
+        self.filter_mapping = {
+            ("LEFT SHOULDER", "LEFT ELBOW"): self.leftShoulderToElbowMvgAvg,
+            ("LEFT ELBOW", "LEFT WRIST"): self.leftElbowToWristMvgAvg,
+            ("RIGHT SHOULDER", "RIGHT ELBOW"): self.rightShoulderToElbowMvgAvg,
+            ("RIGHT ELBOW", "RIGHT WRIST"): self.righttElbowToWristMvgAvg
+        }
+    def normalizeAngle(self, angle:float)->int: #angle_range:tuple=(270,90)
+        mappedAngle = angle - 360
+        mappedAngle %= 360
+        return int(mappedAngle)
+    
     def process_frame(self, img):
+        self.angleContainer.clear()
         # Do pose detection
         results = self.pose.process(img)
         if results is None:
@@ -55,6 +68,8 @@ class PoseDetector:
 
         # Draw landmarks and connections
         try:
+            #TODO Elbow-Wrist angles change with the shoulder - elbow angle change. need a
+            # wat to fix that issue... 
             #Putting Landmark with red circles
             for landmark_idx, _ in self.landmark_mapping.items():
                 landmark = results.pose_landmarks.landmark[landmark_idx]
@@ -66,38 +81,35 @@ class PoseDetector:
             for connection in self.connections:
                 start_landmark = results.pose_landmarks.landmark[connection[0]]
                 end_landmark = results.pose_landmarks.landmark[connection[1]]
+                start_landmark_name = self.landmark_mapping.get(connection[0])
+                end_landmark_name = self.landmark_mapping.get(connection[1])
                 
-                start_x = int(start_landmark.x * img.shape[1])
-                start_y = int(start_landmark.y * img.shape[0])
-                end_x = int(end_landmark.x * img.shape[1])
-                end_y = int(end_landmark.y * img.shape[0])
+                start_x, start_y, end_x, end_y = int(start_landmark.x * img.shape[1]), int(start_landmark.y * img.shape[0]), \
+                                                int(end_landmark.x * img.shape[1]), int(end_landmark.y * img.shape[0])
                 
                 cv2.line(img, (start_x, start_y), (end_x, end_y), (0, 255, 0), 4)
 
                 angle = np.degrees(np.arctan2(end_y - start_y, end_x - start_x))
                 angle = angle + 360 if angle < 0 else angle
-                mapped_angle = map_angle(angle)
-                # Apply rolling average filter to joint angles
-                self.leftShoulderToElbowMvgAvg.add_value(mapped_angle)
-
-                filtered_angle = self.leftShoulderToElbowMvgAvg.get_filtered_value()
+                mapped_angle = self.normalizeAngle(angle)
                 
-                
-                if self.landmark_mapping.get(connection[0]) == "LEFT SHOULDER" and \
-                   self.landmark_mapping.get(connection[1]) == "LEFT ELBOW":
-                    print("{}\t- {}\t: {}".format(self.landmark_mapping.get(connection[0]), \
-                                                  self.landmark_mapping.get(connection[1]), filtered_angle).expandtabs(9))
+                # Apply rolling average filter to joint angles based on landmark names
+                key = (start_landmark_name, end_landmark_name)
+                if key in self.filter_mapping:
+                    self.filter_mapping[key].add_value(mapped_angle)
+                    filtered_angle = self.filter_mapping[key].get_filtered_value()
+                    print("{}\t- {}\t: {}".format(start_landmark_name, end_landmark_name, filtered_angle).expandtabs(9))
+                    self.angleContainer.append(filtered_angle)
+            self.angleContainer[1] = self.angleContainer[1]-self.angleContainer[0]
+            self.angleContainer[3] = self.angleContainer[3]-self.angleContainer[2]
+            print(self.angleContainer)
+        
                 
         except Exception as e:
             print("Error:", e)
         print()
 
         return img
-
-def map_angle(angle):
-    mapped_angle = angle - 270
-    mapped_angle %= 360
-    return mapped_angle
 
 # Example usage:
 def main():
